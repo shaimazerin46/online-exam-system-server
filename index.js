321654f6afa62be6d5522bcf751af77f46e26824
unknown <shaimazerin46@gmail.com>
2025-03-22 22:25:44 +0600

    server side created

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.qkg2o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    const dbCollection = client.db('examinationSystem');
+    const examCollection = dbCollection.collection('exams');
+
+    app.get('/exams', async (req,res)=>{
+        const result = await examCollection.find().toArray();
+        res.send(result)
+    })
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req,res)=>{
+    res.send("server is cooking")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is cooking on port ${port}`)
+})
